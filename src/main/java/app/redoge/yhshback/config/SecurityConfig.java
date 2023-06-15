f12435ef491460a465b7240f438227bf2a77ff6f
Redoge <mail.redoge@gmail.com>
2023-06-15 16:59:46 +0300

    added spring security config (SecurityConfig.java)

--- src/main/java/app/redoge/yhshback/config/SecurityConfig.java
+++ src/main/java/app/redoge/yhshback/config/SecurityConfig.java
@@ -1,2 +1,24 @@
-package app.redoge.yhshback.config;public class SecurityConfig {
+package app.redoge.yhshback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth ->
+                {
+                    try {
+                        auth.requestMatchers("**").permitAll();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }).build();
+    }
 }
