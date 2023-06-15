58293758b8f75a03da3b0df789703c369cc1b9ef
Redoge <mail.redoge@gmail.com>
2023-06-15 20:08:05 +0300

    added UserControllerAdvice

---
+++ src/main/java/app/redoge/yhshback/controller/advice/UserControllerAdvice.java
@@ -0,0 +1,18 @@
+package app.redoge.yhshback.controller.advice;
+
+import app.redoge.yhshback.exception.UserNotFoundException;
+import app.redoge.yhshback.pojo.ErrorPojo;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserControllerAdvice {
+    @ExceptionHandler(value = {UserNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorPojo userNotFoundException(UserNotFoundException ex) {
+        return new ErrorPojo(ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value());
+    }
+}
