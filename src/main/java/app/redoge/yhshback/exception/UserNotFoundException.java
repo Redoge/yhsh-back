a378fdfb5de707ac4b52ca0129fad3d58b81c92f
Redoge <mail.redoge@gmail.com>
2023-06-15 20:07:06 +0300

    added UserNotFoundException

--- src/main/java/app/redoge/yhshback/exception/UserNotFoundException.java
+++ src/main/java/app/redoge/yhshback/exception/UserNotFoundException.java
@@ -1,2 +1,10 @@
-package app.redoge.yhshback.exception;public class UserNotFoundException {
+package app.redoge.yhshback.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException() {
+        super();
+    }
+    public UserNotFoundException(long id) {
+        super("User with id " + id + " not found!!!");
+    }
 }
